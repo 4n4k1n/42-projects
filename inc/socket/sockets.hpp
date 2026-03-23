92c0c07b9dba7af7982428a6da8d9aabad846cef
gzovkic <g.zovkic@web.de>
2026-03-23 17:06:26 +0100

    FEAT(socket/sockets.hpp): added isServerFd, isClientFd, isPOLLIN, isPOLLOUT and isPOLLERR function to header

--- inc/socket/sockets.hpp
+++ inc/socket/sockets.hpp
@@ -35,3 +35,6 @@ void print_poll_fds(std::vector<Connection> &poll_fds);
 // event_loop_if_statments.cpp
 bool isServerFd(Connection con);
 bool isClientFd(Connection con);
+bool isPOLLIN(Connection con);
+bool isPOLLOUT(Connection con);
+bool isPOLLERR(Connection con);
