c68346a7be57618ae0c19d26673a96bfa124452b
Anakin Pregitzer <anakin@pregitzer.de>
2026-03-24 16:49:18 +0100

    Combined all components of the response in the response() function. The methods are still missing

--- src/response/route_matching.cpp
+++ src/response/route_matching.cpp
@@ -6,14 +6,14 @@
 #include "LocationConfig.hpp"
 
 const LocationConfig	*routeMatching(
-	const std::string *uri, const std::vector<LocationConfig> &locations) {
+	const std::string &uri, const std::vector<LocationConfig> &locations) {
 	const LocationConfig	*best = NULL;
 	size_t		best_len = 0;
 
-	for (int i = 0; i < locations.size(); i++) {
+	for (size_t i = 0; i < locations.size(); i++) {
 		const std::string &prefix = locations[i].path;
 
-		if (uri->compare(0, prefix.size(), prefix) == 0 && prefix.size() > best_len) {
+		if (uri.compare(0, prefix.size(), prefix) == 0 && prefix.size() > best_len) {
 				best_len = prefix.size();
 				best = &locations[i];
 		}
