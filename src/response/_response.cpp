551f941bf49636906d166b0bd2e6175ede56178a
Anakin Pregitzer <anakin@pregitzer.de>
2026-03-25 10:48:10 +0100

    Replaced mock data with a actual request

--- src/response/_response.cpp
+++ src/response/_response.cpp
@@ -1,18 +1,19 @@
 #include "response.hpp"
-#include "LocationConfig.hpp"
+#include "Config/LocationConfig.hpp"
 #include "color.hpp"
+#include "GET.hpp"
 
-std::string response(const std::vector<LocationConfig> &locations) {
-	const LocationConfig *loc = routeMatching(std::string("/test"), locations);
+std::string response(const http_request &request, const std::vector<LocationConfig> &locations) {
+	const LocationConfig *loc = routeMatching(request.path, locations);
 
-	if (checkMethod(std::string("GET"), loc) == false) {
+	if (checkMethod(request.method, loc) == false) {
 		std::cerr << RED << "Error: method not allowed!" << RESET << std::endl;
 		return "";
 	}
 
 	std::string file_path(buildRealPath(loc));
 
-	if (checkFile(file_path, std::string("GET")) == -1) {
+	if (checkFile(file_path, request.method) == -1) {
 		std::cerr << RED << "Error: " <<
 			"file does not exists or doesn't have the correct permissions"
 			<< RESET << std::endl;
@@ -20,7 +21,9 @@ std::string response(const std::vector<LocationConfig> &locations) {
 	}
 
 	std::string res = "";
-	// TODO: execute method
+	// TODO: execute method based on request.method
+
+	// res = GET()
 
 	return res;
 }
